5dac67211c582619968bc7ab111c1d0c9aa5d239
luppala <44508859+luppala@users.noreply.github.com>
2022-04-06 00:40:50 -0500

    gene comparison results for PDAC upload

--- compareGenes.py
+++ compareGenes.py
@@ -4,13 +4,14 @@ class compareGenes:
 
     def main(self):
 
-        microbiome_file_name = "GSE14841.top.table-IBS_microbiome.tsv"
+        microbiome_file_name = "GSE19650.top.table-pancreas_microbiome.tsv"
         microbiome_file = open(microbiome_file_name, "r")
 
-        tissue_file_name = "RESULTS-IBS-cleaned.csv"
+        tissue_file_name = "GSE71989.top.table-pancreas_tissue.tsv"
         tissue_file = open(tissue_file_name, "r")
 
-        output_file_name = "RESULTS-IBS-geneCompare.csv"
+        # ***BE SURE to change the output file name!***
+        output_file_name = "RESULTS-PDAC-geneCompare.csv"
         output_file = open(output_file_name, "w")
         output_file.write("gene_name,gene_function\n")
 
@@ -20,10 +21,10 @@ class compareGenes:
         
         for line in tissue_file_list:
 
-            tissue_word = line.split(",")
+            tissue_word = line.split("\t")
             #print(tissue_word)
             
-            tissue_gene = tissue_word[5].strip()
+            tissue_gene = tissue_word[6].strip()
 
             for item in microbiome_file_list:
 
