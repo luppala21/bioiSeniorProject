7f3af3e4afddba3aacb5ba22d2483d483e85b9d2
luppala <44508859+luppala@users.noreply.github.com>
2022-04-06 00:30:19 -0500

    Updated to use for IBS datasets
    use RESULTS-IBS-cleaned.csv for IBS tissue file input

--- compareGenes.py
+++ compareGenes.py
@@ -7,34 +7,36 @@ class compareGenes:
         microbiome_file_name = "GSE14841.top.table-IBS_microbiome.tsv"
         microbiome_file = open(microbiome_file_name, "r")
 
-        tissue_file_name = "RESULTS-IBS.tsv"
+        tissue_file_name = "RESULTS-IBS-cleaned.csv"
         tissue_file = open(tissue_file_name, "r")
 
         output_file_name = "RESULTS-IBS-geneCompare.csv"
         output_file = open(output_file_name, "w")
-        output_file.write("gene_name,gene_function")
+        output_file.write("gene_name,gene_function\n")
 
         microbiome_file_list = microbiome_file.readlines()[1:]
         tissue_file_list = tissue_file.readlines()[1:]
 
-        for item in microbiome_file_list:
+        
+        for line in tissue_file_list:
 
-            microbiome_word = item.split("\t")
-            #print(microbiome_word)
+            tissue_word = line.split(",")
+            #print(tissue_word)
             
-            microbiome_gene = microbiome_word[6].strip()
-            gene_function = microbiome_word[7].strip()
+            tissue_gene = tissue_word[5].strip()
 
-            for line in tissue_file_list:
+            for item in microbiome_file_list:
 
-                tissue_word = line.split("\t")
-                #print(tissue_word)
+                microbiome_word = item.split("\t")
+                #print(microbiome_word)
                 
-                tissue_gene = tissue_word[5].strip()
+                microbiome_gene = microbiome_word[6].strip()
+                gene_function = microbiome_word[7].strip()
 
                 if microbiome_gene == tissue_gene:
 
                     output_file.write(microbiome_gene + "," + gene_function + "\n")
+                    break
 
             
             
