5c95a6ebcebffad4e6e66cd3c4a142c022ed9629
luppala <44508859+luppala@users.noreply.github.com>
2022-04-08 10:01:16 -0500

    Updated to retrieve only significant genes

--- compareGenes.py
+++ compareGenes.py
@@ -1,19 +1,18 @@
 
-
 class compareGenes:
 
     def main(self):
 
-        microbiome_file_name = "GSE19650.top.table-pancreas_microbiome.tsv"
+        microbiome_file_name = "RESULTS-PDAC.csv"
         microbiome_file = open(microbiome_file_name, "r")
 
-        tissue_file_name = "GSE71989.top.table-pancreas_tissue.tsv"
+        tissue_file_name = "RESULTS-IBS.csv"
         tissue_file = open(tissue_file_name, "r")
 
         # ***BE SURE to change the output file name!***
-        output_file_name = "RESULTS-PDAC-geneCompare.csv"
+        output_file_name = "RESULTS-diseases-all-v2.csv"
         output_file = open(output_file_name, "w")
-        output_file.write("gene_name,gene_function\n")
+        output_file.write("gene_name,pvalue,IBS_micro_logFC,PDAC_micro_logFC\n")
 
         microbiome_file_list = microbiome_file.readlines()[1:]
         tissue_file_list = tissue_file.readlines()[1:]
@@ -21,27 +20,33 @@ class compareGenes:
         
         for line in tissue_file_list:
 
-            tissue_word = line.split("\t")
-            #print(tissue_word)
+            tissue_word = line.split(",")
+            print(tissue_word)
             
-            tissue_gene = tissue_word[6].strip()
+            tissue_logFC = float(tissue_word[3].strip())
+            tissue_gene = tissue_word[0].strip()
+            tissue_pvalue = (float(tissue_word[1]) 
+                + float(tissue_word[2]))/2
 
             for item in microbiome_file_list:
 
-                microbiome_word = item.split("\t")
+                microbiome_word = item.split(",")
                 #print(microbiome_word)
                 
-                microbiome_gene = microbiome_word[6].strip()
-                gene_function = microbiome_word[7].strip()
+                microbiome_pvalue = (float(microbiome_word[1]) 
+                    + float(microbiome_word[2]))/2
+                microbiome_logFC = float(microbiome_word[3].strip())
+                microbiome_gene = microbiome_word[0].strip()
+                #gene_function = microbiome_word[1].strip()
 
                 if microbiome_gene == tissue_gene:
+                    pvalue = str((microbiome_pvalue + tissue_pvalue)/2)
 
-                    output_file.write(microbiome_gene + "," + gene_function + "\n")
+                    output_file.write(microbiome_gene + "," 
+                                    + str(microbiome_pvalue) + ","
+                                    + str(tissue_logFC) + ","
+                                    + str(microbiome_logFC) + "\n")
                     break
-
-            
-            
-
 
         tissue_file.close()
         microbiome_file.close()
